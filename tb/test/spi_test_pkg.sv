4b02747ba3cec9e3916d237e82d037484634bc43
Mohamed Adel <m.ad.ali2165@gmail.com>
2021-09-24 09:21:10 +0200

    Implemented spi_test_pkg

---
+++ tb/test/spi_test_pkg.sv
@@ -0,0 +1,14 @@
+package spi_test_pkg;
+
+  import uvm_pkg::*;
+  import apb_agent_pkg::*;
+  import spi_agent_pkg::*;
+  import spi_env_pkg::*;
+  import spi_seq_pkg::*;
+
+  `include "uvm_macros.svh"  
+  `include "spi_test.svh"
+  
+endpackage: spi_test_pkg
+  
+  
